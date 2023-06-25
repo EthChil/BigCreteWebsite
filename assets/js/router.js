fc2880788a6354bba82608d19a9bbb5ed7ef544b
EthChil <echilder@uwaterloo.ca>
2023-06-25 12:42:25 -0700

    fixed router

--- assets/js/router.js
+++ assets/js/router.js
@@ -5,6 +5,12 @@ import {
   router,
 } from "https://unpkg.com/million@1.11.5/dist/router.mjs"
 
+// List of routes that do not include the SPA router
+const nonSPARoutes = [
+  '/construction.html',
+  '/',
+];
+
 export const attachSPARouting = (init, rerender) => {
   // Attach SPA functions to the global Million namespace
   window.Million = {
@@ -13,6 +19,23 @@ export const attachSPARouting = (init, rerender) => {
     prefetch,
     router,
   }
+
+  // Modifying navigation logic
+  document.body.addEventListener('click', event => {
+    if (event.target.tagName === 'A') {
+      // const url = event.target.getAttribute('href');
+      const url = new URL(event.target.href);
+      const path = url.pathname;
+
+      // Prevent default behavior if URL is in nonSPARoutes
+      if (nonSPARoutes.includes(path)) {
+        event.preventDefault();
+
+        // If URL is in nonSPARoutes, do a traditional page load
+        window.location.href = url;
+      }
+    }
+  });
 
   const render = () => requestAnimationFrame(rerender)
   window.addEventListener("DOMContentLoaded", () => {
@@ -23,3 +46,33 @@ export const attachSPARouting = (init, rerender) => {
   })
   window.addEventListener("million:navigate", render)
 }
+
+
+
+// import {
+//   apply,
+//   navigate,
+//   prefetch,
+//   router,
+// } from "https://unpkg.com/million@1.11.5/dist/router.mjs"
+//
+//
+//
+// export const attachSPARouting = (init, rerender) => {
+//   // Attach SPA functions to the global Million namespace
+//   window.Million = {
+//     apply,
+//     navigate,
+//     prefetch,
+//     router,
+//   }
+//
+//   const render = () => requestAnimationFrame(rerender)
+//   window.addEventListener("DOMContentLoaded", () => {
+//     apply((doc) => init(doc))
+//     init()
+//     router(".singlePage")
+//     render()
+//   })
+//   window.addEventListener("million:navigate", render)
+// }
